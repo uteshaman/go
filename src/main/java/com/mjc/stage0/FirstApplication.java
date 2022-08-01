8c345ac347d39e8b834270884f7b13de7d4cd0a5
uteshaman <110390405+uteshaman@users.noreply.github.com>
2022-08-01 16:33:53 +0600

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -3,5 +3,7 @@ package com.mjc.stage0;
 public class FirstApplication {
     public static void main(String[] args) {
         // Write your code here!
+        System.out.println("Hello,World!");
+        
     }
 }
