810d5ebbd840ec17971ab1e44522ba3fa18a54d1
aman <110390405+uteshaman@users.noreply.github.com>
2023-02-19 20:31:51 +0600

    Delete FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++
@@ -1,9 +0,0 @@
-package com.mjc.stage0;
-
-public class FirstApplication {
-    public static void main(String[] args) {
-        // Write your code here!
-        System.out.println("Hello,World!");
-        
-    }
-}
